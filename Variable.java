b415e1e4536a7867315dfd2f7e5ab1f88af03bb9
VasuDev987 <bishwjitgorai1@gmail.com>
2026-01-31 21:18:47 +0530

    variable

---
+++ Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 20;
+
+        System.out.println("The sum of two numbers are : " + (a + b));
+    }
+}
